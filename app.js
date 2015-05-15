0fb7c9d782c018db6d90698c62643e7315939884
alexpark7 <alexpark7@gmail.com>
2015-05-14 18:21:07 -0700

    second commit

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
+var express = require('express');
+var app = express();
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+var path = require('path');
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html' );
 });
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 io.on('connection', function(socket){
-   console.log('user connected');
+    console.log('user connected');
+
+    socket.on('chat message', function( msg ) {
+        io.emit('chat message', msg);
+    });
 });
 
-app.listen(3000, function() {
-    console.log('App listening on port 3000: go to "http://localhost:3000" ')
-});
+http.listen(3000, function() {
+    console.log('go to "http://localhost:3000" ');
+
+})
